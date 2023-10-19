76bf66f95b4bd12e7bc7bc520201984c4e9f2b7b
Eloi Vallverdú <eleloi@gmail.com>
2023-10-19 17:39:53 +0200

    add netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import svelte from "@astrojs/svelte";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind(), svelte()]
+  integrations: [tailwind(), svelte()],
+  adapter: netlify()
 });
